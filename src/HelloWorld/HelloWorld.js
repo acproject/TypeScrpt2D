9fd5a9e1a157dcbfaf98b8b1ae5eba5c680cb364
qinhangyu <acproject@qq.com>
2020-02-13 10:31:06 +0800

    增加tsconfig，将module设置为es2015

--- src/HelloWorld/HelloWorld.js
+++ src/HelloWorld/HelloWorld.js
@@ -1,25 +1,26 @@
-var Canvas2DUtil = /** @class */ (function () {
-    function Canvas2DUtil(canvas) {
-        this.context = canvas.getContext('2d');
-    }
-    Canvas2DUtil.prototype.drawText = function (text) {
-        /**
-         * 因为Canvas2D和WebGL这种底层图API都是状体机模式
-         * 每次绘制前都要调用save将状态进行记录
-         * 然后调用restore将已修改的状态丢到，恢复到初始化状态
-         */
-        this.context.save();
-        this.context.textBaseline = 'middle';
-        this.context.textAlign = 'center';
-        var centerX = this.context.canvas.width * 0.5;
-        var centerY = this.context.canvas.height * 0.5;
-        this.context.fillText(text, centerX, centerY);
-        this.context.strokeStyle = 'green';
-        this.context.strokeText(text, centerX, centerY);
-        this.context.restore();
-    };
-    return Canvas2DUtil;
-}());
+// class Canvas2DUtil {
+//     public context: CanvasRenderingContext2D ;
+//     public constructor(canvas: HTMLCanvasElement) {
+//         this.context = canvas.getContext( '2d' );
+//     }
+//     public drawText(text: string): void {
+//         /**
+//          * 因为Canvas2D和WebGL这种底层图API都是状体机模式
+//          * 每次绘制前都要调用save将状态进行记录
+//          * 然后调用restore将已修改的状态丢到，恢复到初始化状态
+//          */
+//         this.context.save();
+//         this.context.textBaseline = 'middle';
+//         this.context.textAlign = 'center';
+//         let centerX = this.context.canvas.width * 0.5;
+//         let centerY = this.context.canvas.height * 0.5;
+//         this.context.fillText(text ,centerX, centerY);
+//         this.context.strokeStyle = 'green';
+//         this.context.strokeText(text, centerX, centerY);
+//         this.context.restore();
+//     }
+// }
+import { Canvas2DUtil } from '../utils/Canvas2DUtil.js';
 var canvas = document.getElementById('canvas');
 if (canvas === null) {
     alert('无法获取HTMLCanvasElement！');
---
+++ src/utils/Canvas2DUtil.ts
@@ -0,0 +1,29 @@
+export class Canvas2DUtil {
+    
+    public context: CanvasRenderingContext2D | null;
+
+    public constructor(canvas: HTMLCanvasElement) {
+        this.context = canvas.getContext( '2d' );
+    }
+
+    public drawText(text: string): void {
+        /**
+         * 因为Canvas2D和WebGL这种底层图API都是状体机模式
+         * 每次绘制前都要调用save将状态进行记录
+         * 然后调用restore将已修改的状态丢到，恢复到初始化状态
+         */
+        if(this.context!=null) {
+            this.context.save();
+            this.context.textBaseline = 'middle';
+            this.context.textAlign = 'center';
+
+            let centerX = this.context.canvas.width * 0.5;
+            let centerY = this.context.canvas.height * 0.5;
+
+            this.context.fillText(text ,centerX, centerY);
+            this.context.strokeStyle = 'green';
+            this.context.strokeText(text, centerX, centerY);
+            this.context.restore();
+        }
+    }
+}
